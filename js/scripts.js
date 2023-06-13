66d467b7ac9838a8b118dfc3b1668282b4e9a75f
stiva98 <samustiva9810@gmail.com>
2023-06-13 10:09:22 +0200

    cambiato stringhe in numeri ad age e kmTraveled

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-const kmTraveled = prompt ('Quanti kilometri deve percorrere?');
+const kmTraveled = parseInt(prompt ('Quanti kilometri deve percorrere?'));
 //console.log ('kmTraveled', kmTraveled, typeof kmTraveled );
 
-const age = prompt ('Qual è la sua età?');
+const age = parseInt(prompt ('Qual è la sua età?'));
 //console.log ('age', age, typeof age );
 
 document.getElementById('ticket').innerHTML = kmTraveled + ' km';
